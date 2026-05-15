de0eb02bb5854075a9ddf86962dda8d6464bc68c
Parth Patil <parthhpatil2005@gmail.com>
2026-05-15 13:42:24 +0530

    Final commit

--- server/server.js
+++ server/server.js
@@ -24,6 +24,6 @@ app.use('/api/auth', authRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT,'0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
